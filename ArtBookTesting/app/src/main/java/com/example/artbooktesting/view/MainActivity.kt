9291fe2dba5935416493a6c6a8bad7c3022a0efa
Emre USTA <emre.usta@stu.fsm.edu.tr>
2021-10-31 00:59:00 +0300

    navigation and viewbinding implemented

--- ArtBookTesting/app/src/main/java/com/example/artbooktesting/view/MainActivity.kt
+++ ArtBookTesting/app/src/main/java/com/example/artbooktesting/view/MainActivity.kt
@@ -1,9 +1,10 @@
- package com.example.artbooktesting
+ package com.example.artbooktesting.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.artbooktesting.R
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
